d8a8a801a61447be61c6e9523f87c6128999798b
Davi9990 <davi.caridade13@gmail.com>
2024-09-13 21:17:41 -0300

    Foi adicionado as telas

--- banco-dados.sql
+++ banco-dados.sql
@@ -1,4 +1,6 @@
 create table usuario (
     usuario_id INT PRIMARY KEY AUTO_INCREMENT,
-    Cu
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    senha VARCHAR(255) NOT NULL
 );

84f264bc77d5ed8e47e9a6a0ef1dbe53f0418b7f
Paulo Victor Costa <paulo.v.melo@ba.estudante.senai.br>
2024-09-13 21:05:13 -0300

    :gear: Comando SQL para criação do banco de dados e da tabela usuario

--- banco-dados.sql
+++ banco-dados.sql
@@ -1,4 +1,12 @@
+create database crudLoginPHP;
+use crudLoginPHP;
+
 create table usuario (
-    usuario_id INT PRIMARY KEY AUTO_INCREMENT,
-    no
+	usuario_id int primary key auto_increment,
+    nome varchar (255) not null,
+    telefone varchar(11) not null unique,
+    email varchar(255) not null unique,
+    cpf varchar(11) not null unique,
+    senha varchar(255) not null,
+    confirmar_senha varchar(255) not null
 );

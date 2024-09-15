39ee1155de05c78ab458e90ba632159303624435
Paulo Victor <victorcosta.bsi@gmail.com>
2024-09-15 14:12:58 -0300

    :bulb: Comentários: Foram inseridos documentação de autoria

--- src/configuracoes/processar.php
+++ src/configuracoes/processar.php
@@ -1,4 +1,13 @@
 <?php
 
+/**
+ * CURSO TÉCNICO EM DESENVOLVIMENTO DE SISTEMAS
+ * @author paulo.v.melo@ba.estudante.senai.br
+ * @author davi.caridade@ba.estudante.senai.br
+ */
+
+
 require_once 'vendor/autoload.php';
+
+
 

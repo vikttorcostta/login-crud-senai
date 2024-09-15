db63920c484c96203bfd0cb4b50b9bb70a614774
Paulo Victor <victorcosta.bsi@gmail.com>
2024-09-15 14:12:05 -0300

    :bulb: Comentários: Foram inseridos documentação de autoria

--- src/configuracoes/configuracoes.php
+++ src/configuracoes/configuracoes.php
@@ -1,6 +1,12 @@
 <?php
 
-const DBNAME = '';
+/**
+ * CURSO TÉCNICO EM DESENVOLVIMENTO DE SISTEMAS
+ * @author paulo.v.melo@ba.estudante.senai.br
+ * @author davi.caridade@ba.estudante.senai.br
+ */
+
+const DBNAME = 'crudLoginPHP';
 const HOSTNAME = 'localhost';
 const USERNAME = 'root';
 const PASSWORD = '';

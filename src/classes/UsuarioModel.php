633f4ab6eab4e696b20d893324487ce0fe88d306
Davi9990 <162383055+Davi9990@users.noreply.github.com>
2024-09-16 14:46:29 -0300

    Atualização9

--- src/classes/UsuarioModel.php
+++ src/classes/UsuarioModel.php
@@ -1,7 +1,12 @@
 <?php
 
-namespace Senai\CrudPhp\classes;
+/**
+ * CURSO TÉCNICO EM DESENVOLVIMENTO DE SISTEMAS
+ * @author paulo.v.melo@ba.estudante.senai.br
+ * @author davi.caridade@ba.estudante.senai.br1
+ */
 
+namespace Senai\CrudPhp\classes;
 use PDO;
 
 class UsuarioModel
@@ -11,158 +16,137 @@ class UsuarioModel
     private string $telefone;
     private string $cpf;
     private string $senha;
-    private string $confirmarSenha;
-    private const ENTIDADE = 'usuario';
-    private $fail;
-    
-    //private string $password;
-    private string $EmailLogin;
+    private const string ENTIDADE = 'usuario';
+    private PDO $connection;
 
-    public function __construct(string $nome = null, string $email = null, string $telefone = null, int $cpf = null, string $senha = null,string $EmailLogin = null){
-        
-        $this->nome = $nome;
-        $this->email = $email;
-        $this->telefone = $telefone;
-        $this->cpf = $cpf;
-        $this->senha = $senha;
-        //$this->password = $password;
-        $this->EmailLogin = $EmailLogin;
-        //$this->confirmarSenha = $confirmarSenha;
+    public function __construct(){
+
+       $this->connection = BancoDeDados::getInstance()->getConnection();
+
     }
 
-    public function insertCadastro(): bool{
-        // Implementar a lógica de inserção do usuário no banco de dados(Cadastro)
+    public function cadastrar(string $nome, string $email, string $telefone, string $cpf, string $senha): bool{
 
-        if($this->checando()){
-            $this->fail = 'Necessario preencher todos os campos acima';
-            return false;
-        }
+        $senhaHasheada = $this->senha = password_hash($this->senha, PASSWORD_DEFAULT);
 
-        if(!$this->VerificarSenha()){
-            $this->fail = 'As senhas não são iguais';
-            return false;
-        }
-
-        if($this->cadastroUnico()){
-            $this->fail = 'Alguns dos campos acima já foi cadastrados';
-            return false;
-        }
-
-       
-
-        // Hash da senha antes de armazenar no banco de dados
-        $hashedPassword = password_hash($this->senha, PASSWORD_DEFAULT);
-
-        $pdo = BancoDeDados::__construct();
-        $res = $pdo->prepare(query: "INSERT INTO". self::ENTIDADE. "(nome,telefone,email,cpf,senha,confirmarSenha) VALUES (:nome,:telefone,:email,:cpf,:senha)");
-        $res->bindParam(':nome', $this->nome);
-        $res->bindParam(':telefone', $this->telefone);
-        $res->bindParam(':email', $this->email);
-        $res->bindParam(':cpf', $this->cpf);
-        $res->bindParam(':senha', $hashedPassword);
-        //$res->bindParam(':confirmarSenha', $this->confirmarSenha);
-        return $res->execute();
-        echo "Usuário inserido com sucesso!";
+        $pdo = "INSERT INTO " .self::ENTIDADE ."(nome, email, telefone, cpf, senha)" . "VALUES(:nome, :email, :telefone, :cpf, :senha)";
+        $estado = $this->connection->prepare($pdo);
+        $estado->bindParam(':nome', $this->nome);
+        $estado->bindParam(':email', $this->email);
+        $estado->bindParam(':telefone', $this->telefone);
+        $estado->bindParam(':cpf', $this->cpf);
+        $estado->bindParam(':senha', $senhaHasheada);
+        return $estado->execute();
     }
 
-    public static function listar(): array {
-         // Implementar a lógica de listagem dos usuários no banco de dados
-         $pdo = BancoDeDados::__construct();
-         $res = $pdo->query("SELECT * FROM". self::ENTIDADE);
-         return $res->fetchAll(PDO::FETCH_ASSOC);
-         echo "Listando usuários!";
+    /** LISTAGEM DE USUÁRIOS
+     * @return array
+     */
+    public function listar(): array {
+        $pdo = "SELECT * FROM " . self::ENTIDADE;
+        $estado = $this->connection->prepare($pdo);
+        $estado->execute();
+        return $estado->fetchAll();
     }
 
-    public function login(){
-         // Implementar a lógica de login do usuário no banco de dados
-         // Checar se os campos de login foram preenchidos
-    if ($this->checandoLogin()) {
-        $this->fail = 'Preencha todos os campos.';
+    /** AUTENTICAÇÃO DE CADASTRO
+     * @return bool
+     */
+    public function authCadastro(): bool
+    {
+        $pdo = "SELECT email FROM " . self::ENTIDADE . "WHERE email = :email";
+        $estado = $this->connection->prepare($pdo);
+        $estado->bindParam(':email', $this->email);
+        $estado->execute();
+        if($estado->rowCount() > 0)return true;
         return false;
     }
 
-    // Buscar o usuário no banco de dados com base no email
-    $pdo = BancoDeDados::__construct();
-    $res = $pdo->prepare("SELECT * FROM " . self::ENTIDADE . " WHERE email = :email");
-    $res->bindParam(':email', $this->EmailLogin);
-    $res->execute();
+    /** AUTENTICAÇÃO DE USUÁRIO
+     * @return bool
+     */
+    public function authLogin(): bool
+    {
+        $pdo = "SELECT email, senha FROM " . self::ENTIDADE . " WHERE email = :email";
+        $estado = $this->connection->prepare($pdo);
+        $estado->bindParam(':email', $this->email);
+        $estado->execute();
 
-    // Se o usuário existir, verificar a senha
-    if ($res->rowCount() === 1) {
-        $usuario = $res->fetch(PDO::FETCH_ASSOC);
-        
-        // Verificar a senha usando password_verify
-        if (password_verify($this->senha, $usuario['senha'])) {
-            // Sucesso no login, aqui você pode iniciar uma sessão, se necessário
-            echo "Login realizado com sucesso!";
-            return true;
-        } else {
-            $this->fail = 'Senha incorreta.';
-            return false;
+        if ($estado->rowCount() > 0) {
+            $usuario = $estado->fetch(PDO::FETCH_ASSOC);
+            if(password_verify($this->senha, $usuario['senha'])) return true;
         }
-    } else {
-        $this->fail = 'Usuário não encontrado.';
         return false;
     }
-    }
 
-    public function VerificarSenha(): bool{
-        //Implementar a lógica de verificação de senha no banco de dados
-        if($this->senha === $this->confirmarSenha){
-            return true;
-        }else{
-            $this->fail = "Senhas não conferem!";
-            return false;
-        }
-    }
-
-    public function VerificarCPF(){
-        //Implementar a lógica de verificação de senha no banco
+    /** VALIDAÇÃO DOS CAMPOS DE CADASTRO
+     * @param string $nome
+     * @param string $email
+     * @param string $telefone
+     * @param string $cpf
+     * @param string $senha
+     * @param string $confirmarSenha
+     * @return bool
+     */
+    public function isCadastro(string $nome, string $email, string $telefone, string $cpf, string $senha, string $confirmarSenha): bool
+    {
         return true;
     }
 
-    public function cadastrounico(){
-        // Implementar a lógica de verificação se o email e CPF, e outros campos, já estão cadastrados no banco de dados
-        $pdo = BancoDeDados::__construct();
-        $res = $pdo->prepare("SELECT * FROM ".self::ENTIDADE."WHERE nome = :nome OR telefone = :telefone OR email = :email, cpf = :cpf OR senha = :senha");
-        $res->bindValue(':nome', $this->nome);
-        $res->bindValue(':telefone', $this->telefone);
-        $res->bindValue(':email', $this->email);
-        $res->bindValue(':cpf', $this->cpf);
-        $res->bindValue(':senha', $this->senha);
-        $res->execute();
-        if($res->rowCount() > 0){
-            return true;
-        }
-        return false;
+    /** VALIDAÇÃO DOS CAMPOS DE LOGIN
+     * @return bool
+     */
+    public function isLogin(): bool
+    {
+        return $this->isConfirmarSenha(1);
     }
 
-    public function checando(){
-        //Impede o usuario de deixar algum campo vazio na hota do cadastro
-        if(empty($this->nome) || empty($this->telefone) || empty($this->email)  || empty($this->cpf)  || empty($this->senha) ){
-            return true;
-        }
-        return false;
+    /** VALIDAÇÃO DA CONFIRMAÇÃO DE SENHA
+     * @param string $confirmarSenha
+     * @return bool
+     */
+    public function isConfirmarSenha(string $confirmarSenha): bool
+    {
+        return $this->senha === $confirmarSenha;
     }
 
-    public function LoginUnico(){
-        $pdo =  BancoDeDados::__construct();
-        $res = $pdo->prepare("SELECT * FROM ".self::ENTIDADE." WHERE Email = :Email or senhaLogin = :senhaLogin");
-        $res->bindValue(':Email', $this->email);
-        $res->bindValue(':senhaLogin', $this->senha);
-        $res->execute();
-        if($res->rowCount() > 0){
-            return true;
-        }
-        return false;
+    /** VALIDAÇÃO E LIMPEZA DE NOME
+     * @return bool
+     */
+    public function isNome(): bool
+    {
+        return true;
     }
 
-    public function checandoLogin(){
-        // Impede o usuario de deixar algum campo vazio na hora do login
-        if(empty($this->Email)  || empty($this->senhaLogin) ){
-            return true;
-        }
-        return false;
+    /** VALIDAÇÃO E LIMPEZA DO CPF
+     * @return bool
+     */
+    public function isCPF(): bool
+    {
+        return true;
     }
 
+    /** VALIDAÇÃO E LIMPEZA DE TELEFONE
+     * @return bool
+     */
+    public function isTelefone(): bool
+    {
+        return true;
+    }
+
+    /** VALIDAÇÃO E LIMPEZA DE E-MAIL
+     * @return bool
+     */
+    public function isEmail(): bool
+    {
+        return true;
+    }
+
+    /** VALIDAÇÃO E LIMPEZA DE SENHA
+     * @return bool
+     */
+    public function isSenha(): bool
+    {
+        return true;
+    }
 }
